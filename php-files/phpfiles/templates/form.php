a9411098b1fa5b650c6baf6bc7168d0b91d3fd4c
Nast <blackstonesword@mail.ru>
2019-03-15 11:12:21 +0400

    No changes

---
+++ php-files/phpfiles/templates/form.php
@@ -0,0 +1,8 @@
+<div class="wide">
+    <form action="#" method="POST" enctype="multipart/form-data">
+	<h4>Загрузите файл на&nbsp;сервер</h4>
+	<input type="file" name="photo[]" multiple accept="image/*"><br>
+	<input type="submit" value="Сохранить">
+	
+    </form>
+</div>
