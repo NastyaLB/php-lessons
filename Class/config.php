06b1271ee0af04d91450bb14a43218103f80d82b
Nast <blackstonesword@mail.ru>
2019-05-15 01:20:09 +0400

    устарели

--- Class/config.php
+++
@@ -1,6 +0,0 @@
-<?
-$server="localhost";
-$login="root";
-$pass="";
-$db="lesson2-2";
-$connect=mysqli_connect($server,$login,$pass,$db);
--- Class/index.php
+++
@@ -1,6 +0,0 @@
-<?
-// подгружаем и активируем авто-загрузчик Twig-а
-include 'Twig/Autoloader.php';
-Twig_Autoloader::register();
-
-include_once('views/views.php');
