ef109a7eb0beb001c8c2e7be7be8ac7bf0123eeb
Nast <blackstonesword@mail.ru>
2019-05-15 01:22:55 +0400

    изменены условия назначения классов

---
+++ 06_final_system/c/C_Page.php
@@ -0,0 +1,32 @@
+<?php
+//
+// ??????????? ???????? ??????.
+//
+include_once('m/model.php');
+
+class C_Page extends C_Base
+{
+	//
+	// ???????????.
+	//
+	
+	public function action_index(){
+		$this->title .= '::Reeding';
+		$text = text_get();
+		$this->content = $this->Template('v/v_index.php', array('text' => $text));	
+	}
+	
+	public function action_edit(){
+		$this->title .= '::Redacting';
+		
+		if($this->isPost())
+		{
+			text_set($_POST['text']);
+			header('location: index.php');
+			exit();
+		}
+		
+		$text = text_get();
+		$this->content = $this->Template('v/v_edit.php', array('text' => $text));		
+	}
+} 
