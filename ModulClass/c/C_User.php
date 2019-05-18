5403ee16ee21d800322546ba941fc48a50082e36
Nast <blackstonesword@mail.ru>
2019-05-18 11:40:17 +0400

    класс юзера

---
+++ ModulClass/c/C_User.php
@@ -0,0 +1,44 @@
+<?php
+//
+// Конттроллер страницы чтения.
+//
+include_once('m/model.php');
+
+class C_User extends C_Base
+{
+	//
+	// Конструктор.
+	//
+	
+	public function action_cabinet(){	
+		$this->title .= '::Личный кабинет';          
+		$text = $this->userlink[form];
+		
+		if($this->isPost())
+		{
+            $_SESSION[username]=null;
+            $_SESSION[password]=null;
+            $_SESSION[$arres]=null;
+            session_destroy();
+			header('location: index.php');
+			exit();
+		}
+		$this->content = $this->Template('v/v_index.php', array('text' => $text));
+	}
+	
+	
+	public function action_login($connect){
+		$this->title .= '::Вход в личный кабинет';
+        
+        if($this->isPost())
+		{
+            if(!isset($_SESSION[username])&&isset($_POST[username])) {
+                checkPass($connect);
+            }
+			header('location: index.php');
+			exit();
+		}
+        
+		$this->content = $this->Template('v/v_login.php');
+	}
+} 
