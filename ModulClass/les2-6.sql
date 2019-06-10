ae9bb124fc30d256d31addefce927f0c4460840b
Nast <blackstonesword@mail.ru>
2019-06-10 20:56:34 +0400

    БД

--- ModulClass/les2-6.sql
+++ ModulClass/les2-6.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 22 2019 г., 16:14
+-- Время создания: Июн 10 2019 г., 19:50
 -- Версия сервера: 5.6.41
 -- Версия PHP: 5.6.38
 
@@ -47,7 +47,11 @@ INSERT INTO `basket` (`id_basket`, `id_user`, `id_good`, `number`, `price`, `is_
 (2, 1, 2, 1, 350, 1, 1),
 (3, 1, 1, 1, 400, 1, 2),
 (4, 1, 3, 1, 450, 1, 2),
-(5, 1, 5, 1, 500, 1, 3);
+(5, 1, 5, 1, 500, 1, 3),
+(6, 2, 3, 1, 450, 1, 4),
+(7, 2, 4, 1, 300, 1, 4),
+(8, 2, 2, 2, 350, 1, 5),
+(9, 2, 4, 1, 300, 1, 5);
 
 -- --------------------------------------------------------
 
@@ -93,7 +97,12 @@ INSERT INTO `goods` (`id_good`, `good_name`, `price`, `id_category`, `charact`, 
 (2, 'Плюшевый заяц', 350, 1, 'Трогательный плюшевый зайчик с длинными ушами', 1),
 (3, 'Плюшевый медведь', 450, 1, 'Большой дружелюбный мишка. Мягкий и пушистый', 1),
 (4, 'Бархатный кролик', 300, 1, 'Приятный на ощупь бархатный крошка-кролик ', 1),
-(5, 'Плюшевый слон', 500, 1, 'Плюшевый слон с огромными мягкими ушами', 1);
+(5, 'Плюшевый слон', 500, 1, 'Плюшевый слон с огромными мягкими ушами', 1),
+(6, 'Плюшевый котенок', 320, 1, 'Плюшевый котенок  Озорной котёнок. Мягкие лапки, игривая мордочка, пушистый хвост', 0),
+(7, 'Плюшевый дельфин', 420, 1, 'Весёлый плюшевый дельфинчик с белым брюшком', 1),
+(8, 'Плюшевая коала', 370, 1, 'Плюшевая коала с меховыми щечками', 1),
+(9, 'Плюшевый хомяк', 270, 1, 'Плюшевый кроха-хомяк с цепкими, но мягкими лапками', 1),
+(10, 'Плюшевая сова', 300, 1, 'Плюшевая совушка. Мягкая, но мудрая красотка с бантиком', 1);
 
 -- --------------------------------------------------------
 
@@ -106,17 +115,19 @@ CREATE TABLE `orders` (
   `id_user` int(11) NOT NULL,
   `amount` double NOT NULL,
   `datetime_create` datetime NOT NULL,
-  `id_orederstatus` int(11) NOT NULL
+  `id_ordrstatus` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `orders`
 --
 
-INSERT INTO `orders` (`id_order`, `id_user`, `amount`, `datetime_create`, `id_orederstatus`) VALUES
-(1, 1, 750, '0000-00-00 00:00:00', 1),
+INSERT INTO `orders` (`id_order`, `id_user`, `amount`, `datetime_create`, `id_ordrstatus`) VALUES
+(1, 1, 750, '0000-00-00 00:00:00', 2),
 (2, 1, 950, '0000-00-00 00:00:00', 1),
-(3, 1, 500, '0000-00-00 00:00:00', 1);
+(3, 1, 500, '0000-00-00 00:00:00', 1),
+(4, 2, 750, '0000-00-00 00:00:00', 0),
+(5, 2, 1000, '0000-00-00 00:00:00', 0);
 
 -- --------------------------------------------------------
 
@@ -134,8 +145,10 @@ CREATE TABLE `order_status` (
 --
 
 INSERT INTO `order_status` (`id_orderstatus`, `order_status_name`) VALUES
+(0, 'new'),
 (1, 'inprocess'),
-(2, 'done');
+(2, 'sended'),
+(3, 'done');
 
 -- --------------------------------------------------------
 
@@ -167,9 +180,10 @@ CREATE TABLE `role` (
 
 CREATE TABLE `users` (
   `id_user` int(11) NOT NULL,
-  `user_name` varchar(50) NOT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
   `user_login` varchar(50) NOT NULL,
   `user_pass` varchar(50) NOT NULL,
+  `address` varchar(128) DEFAULT NULL,
   `user_lastact` timestamp NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -177,8 +191,11 @@ CREATE TABLE `users` (
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id_user`, `user_name`, `user_login`, `user_pass`, `user_lastact`) VALUES
-(1, 'Соня Механикова', 'Some', 'SomeSome', '0000-00-00 00:00:00');
+INSERT INTO `users` (`id_user`, `user_name`, `user_login`, `user_pass`, `address`, `user_lastact`) VALUES
+(0, 'admin', 'admin', 'admin', NULL, '0000-00-00 00:00:00'),
+(1, 'Соня Механикова', 'Some', 'SomeSome', '', '0000-00-00 00:00:00'),
+(2, 'Рита Таурова', 'Ruta', 'RutaRuta', 'ул.Калинина,25', '0000-00-00 00:00:00'),
+(3, '', 'u45079', 'u45079', '', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -203,7 +220,7 @@ ALTER TABLE `basket`
   ADD PRIMARY KEY (`id_basket`),
   ADD KEY `id_user` (`id_user`),
   ADD KEY `id_good` (`id_good`),
-  ADD KEY `id_order` (`id_order`);
+  ADD KEY `basket_ibfk_3` (`id_order`);
 
 --
 -- Индексы таблицы `categories`
@@ -225,7 +242,7 @@ ALTER TABLE `goods`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id_order`),
-  ADD KEY `orders_ibfk_1` (`id_orederstatus`),
+  ADD KEY `orders_ibfk_1` (`id_ordrstatus`),
   ADD KEY `id_user` (`id_user`);
 
 --
@@ -271,7 +288,7 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT для таблицы `basket`
 --
 ALTER TABLE `basket`
-  MODIFY `id_basket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_basket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT для таблицы `categories`
@@ -283,19 +300,19 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT для таблицы `goods`
 --
 ALTER TABLE `goods`
-  MODIFY `id_good` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_good` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT для таблицы `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `order_status`
 --
 ALTER TABLE `order_status`
-  MODIFY `id_orderstatus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_orderstatus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `pages`
@@ -313,7 +330,7 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `user_role`
@@ -331,7 +348,7 @@ ALTER TABLE `user_role`
 ALTER TABLE `basket`
   ADD CONSTRAINT `basket_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
   ADD CONSTRAINT `basket_ibfk_2` FOREIGN KEY (`id_good`) REFERENCES `goods` (`id_good`),
-  ADD CONSTRAINT `basket_ibfk_3` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id_order`);
+  ADD CONSTRAINT `basket_ibfk_3` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id_order`) ON DELETE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `goods`
@@ -343,7 +360,7 @@ ALTER TABLE `goods`
 -- Ограничения внешнего ключа таблицы `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`id_orederstatus`) REFERENCES `order_status` (`id_orderstatus`),
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`id_ordrstatus`) REFERENCES `order_status` (`id_orderstatus`),
   ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
 
 --
