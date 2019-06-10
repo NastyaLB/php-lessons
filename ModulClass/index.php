8d41f5c131aafb5c71152f3d593c6a9dad396279
Nast <blackstonesword@mail.ru>
2019-06-10 20:56:44 +0400

    какие-то изменения

--- ModulClass/index.php
+++ ModulClass/index.php
@@ -1,5 +1,6 @@
 <?php
 session_start(); //session_destroy();
+$_SESSION[buyItem][0][id][0] = 'Мы будем рады, если вы зарегистрируетесь';
 include('config.php');
 
 function __autoload($classname){
@@ -20,8 +21,16 @@ switch ($action)
 	case 'action_view':
 		$controller = new C_Good();
 		break;
+	case 'action_order':
+		$controller = new C_Order();
+		break;
+	case 'action_admnstr':
+		$controller = new C_Admin();
+	case 'action_store':
+		$controller = new C_Admin();
+		break;
 	default:
-		$controller = new C_Page();
+        $controller = new C_Page();
 }
 
 $controller->Request($action);
